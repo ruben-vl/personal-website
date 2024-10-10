dde2470f1f2485214ac77c9d782abb42632d82a8
ruben-vl <rubenvanlaer@mail.com>
2024-10-10 16:10:38 +0200

    Add order

---
+++ js/script.js
@@ -0,0 +1,7 @@
+toggles = document.querySelectorAll(".toggle");
+
+toggles.forEach(toggle => toggle.addEventListener("click", (e) => console.log(e.target.textContent)))
+
+function hide_toggles() {
+    
+}

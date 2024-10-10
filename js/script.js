1b5dcfef0fd1516bafc9643ae662c4535100766f
ruben-vl <rubenvanlaer@mail.com>
2024-10-10 16:44:07 +0200

    Fix viewport width to rem

--- js/script.js
+++ js/script.js
@@ -2,14 +2,12 @@ toggles = document.querySelectorAll(".toggle");
 toggles.forEach(toggle => toggle.addEventListener("click", () => toggleClick(toggle)))
 
 search_icon = document.querySelector("#search-icon");
-search_icon.addEventListener("click", () => toggleToggles());
+search_icon.addEventListener("click", () => toggleTogglesVisibility());
 
 function toggleClick(target) {
     target.classList.toggle("highlighted");
 }
 
-function toggleToggles() {
-    console.log("Attempting to hide toggles");
+function toggleTogglesVisibility() {
     document.body.classList.toggle("hide-toggle");
-    // toggles.forEach(toggle => toggle.classList.toggle("hide-toggle"));
 }

55f2ae1604ba152b2f1fb93b50c4b594eb3527f9
ruben-vl <rubenvanlaer@mail.com>
2024-10-16 17:23:21 +0200

    Remove unused js

--- js/script.js
+++ js/script.js
@@ -1,50 +1,50 @@
-toggles = document.querySelectorAll(".toggle");
-toggles.forEach(toggle => toggle.addEventListener("click", () => toggleClick(toggle)))
+// toggles = document.querySelectorAll(".toggle");
+// toggles.forEach(toggle => toggle.addEventListener("click", () => toggleClick(toggle)))
 
-// search_icon = document.querySelector("#search-icon");
-// search_icon.addEventListener("click", () => toggleTogglesVisibility());
+// // search_icon = document.querySelector("#search-icon");
+// // search_icon.addEventListener("click", () => toggleTogglesVisibility());
 
-content_boxes = document.querySelectorAll(".content-box");
+// content_boxes = document.querySelectorAll(".content-box");
 
-function toggleClick(target) {
-    target.classList.toggle("highlighted");
-    if (target.classList.contains("highlighted")) {
-        addContentHighlighting(target.textContent);
-    } else {
-        removeContentHighlighting();
-        addHighlightingForActiveToggles();
-    }
-}
+// function toggleClick(target) {
+//     target.classList.toggle("highlighted");
+//     if (target.classList.contains("highlighted")) {
+//         addContentHighlighting(target.textContent);
+//     } else {
+//         removeContentHighlighting();
+//         addHighlightingForActiveToggles();
+//     }
+// }
 
-function addContentHighlighting(toggleText) {
-    content_boxes.forEach(content_box => {
-        if (content_box.dataset.toggles.includes(toggleText)) {
-            content_box.classList.add("highlighted");
-        }
-    });
-}
+// function addContentHighlighting(toggleText) {
+//     content_boxes.forEach(content_box => {
+//         if (content_box.dataset.toggles.includes(toggleText)) {
+//             content_box.classList.add("highlighted");
+//         }
+//     });
+// }
 
-function addHighlightingForActiveToggles() {
-    toggles.forEach(toggle => {
-        if (toggle.classList.contains("highlighted")) {
-            addContentHighlighting(toggle.textContent);
-        }
-    });
-}
+// function addHighlightingForActiveToggles() {
+//     toggles.forEach(toggle => {
+//         if (toggle.classList.contains("highlighted")) {
+//             addContentHighlighting(toggle.textContent);
+//         }
+//     });
+// }
 
-function removeContentHighlighting() {
-    content_boxes.forEach(content_box => content_box.classList.remove("highlighted"));
-}
+// function removeContentHighlighting() {
+//     content_boxes.forEach(content_box => content_box.classList.remove("highlighted"));
+// }
 
-function removeToggleHighlighting() {
-    toggles.forEach(toggle => toggle.classList.remove("highlighted"));
-}
+// function removeToggleHighlighting() {
+//     toggles.forEach(toggle => toggle.classList.remove("highlighted"));
+// }
 
-function toggleTogglesVisibility() {
-    document.body.classList.toggle("hide-toggle");
-    removeContentHighlighting();
-    removeToggleHighlighting();
-}
+// function toggleTogglesVisibility() {
+//     document.body.classList.toggle("hide-toggle");
+//     removeContentHighlighting();
+//     removeToggleHighlighting();
+// }
 
 
 // THEME TOGGLE
